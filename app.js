2ca2d817be8328158fc211442da4c5054de1bc91
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-01-31 10:38:04 +0300

    template engine

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ app.set("view engine","ejs");
 app.use(express.static('public'));
 
 
-
+//ROUTES
 app.get('/', (req, res) => {
-  //res.sendFile(path.resolve(__dirname,'template/index.html'));
   res.render('index')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
 });
 
 const port = 3000;
