e77a1171c665dd701a5bc8988ff67520c84e4207
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-02-04 17:47:22 +0300

    add photos informations and connnect db

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const ejs=require('ejs');
 const path=require('path');
 const app = express();
+const Photo=require('./models/Photo')
+const mongoose = require('mongoose');
+
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set("view engine","ejs");
 //MIDDLEWARES
 
 app.use(express.static('public'));
-
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -20,6 +30,14 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add')
 });
+
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
+});
+
+
 
 const port = 3000;
 
