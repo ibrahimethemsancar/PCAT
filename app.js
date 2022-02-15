ca93ce8f8889d41c6c8dbc9c70927afa1c40eb9c
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-02-15 12:14:22 +0300

    delete a photo

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'));
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
 app.get('/', async (req, res) => {
@@ -79,14 +83,23 @@ app.get('/photos/edit/:id', async (req, res) => {
   });
 });
 
-app.put('/photos/:id',async(req,res)=>{
-  const photo=await Photo.findOne({_id:req.params.id});
-  photo.title=req.body.title;
-  photo.description=req.body.description;
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
   photo.save();
 
-  res.redirect(`/photos/${photo._id}`)
-})
+  res.redirect(`/photos/${photo._id}`);
+});
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
+});
+
 const port = 3000;
 
 app.listen(port, () => {
